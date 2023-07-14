8f9113d1272fd27673f72b8eb263971fb864aa5a
Albert Dias <albert.uft@gmail.com>
2023-07-13 23:28:06 -0300

    first lesson flutter devquiz

---
+++ lib/home/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:DevQuiz/home/widgets/appbar/app_bar_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBarWidget(),
+    );
+  }
+}
